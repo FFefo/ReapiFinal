9bb7c4f632a73be9a4693e77b61b1e025969a956
gabbiee33 <gabriellepereirarocha96@gmail.com>
2024-10-05 21:45:43 -0300

    consultar feitos

--- src/pages/consultarCanalPrograma/consultaprograma.jsx
+++ src/pages/consultarCanalPrograma/consultaprograma.jsx
@@ -1,10 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Cabecalho from '../../components/cabecalho';
 import './consultaprograma.scss'
 
 export default function ConsultarPrograma() {
+
+    const [programa, setPrograma] = useState([]);
+
+        async function Buscar() {
+        const url = 'http://localhost:3200/canalPrograma/';
+        let resp = await axios.get(url);
+        setPrograma(resp.data);
+        }
+
     return (
         <div className='pagina-consultar-programa'>
             <Cabecalho />
+
+            <h1>Consultar Programas</h1>
+
+            <button onClick={Buscar}>Buscar</button>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Canal ID</th>
+                        <th>Nome</th>
+                        <th>Genêro</th>
+                        <th>Horário</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {programa.map(item =>
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.canal}</td>
+                            <td>{item.nome}</td>
+                            <td>{item.genero}</td>
+                            <td>{item.horario}</td>
+                            <td><Link to= {`/cadastrar/${item.id}`}>Alterar</Link></td>
+                        </tr>
+                    )}
+                </tbody>
+
+            </table>
 
         </div>
     )
--- src/pages/consultarUsuario/consultausuario.jsx
+++ src/pages/consultarUsuario/consultausuario.jsx
@@ -1,10 +1,46 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Cabecalho from '../../components/cabecalho';
 import './consultausuario.scss'
 
 export default function ConsultarUsuario() {
+
+    const [usuario, setUsuario] = useState([]);
+
+        async function Buscar() {
+        const url = 'http://localhost:3200/usuario/';
+        let resp = await axios.get(url);
+        setUsuario(resp.data);
+        }
+
     return (
         <div className='pagina-consultar-usuario'>
             <Cabecalho />
+
+            <h1>Consultar Usuários</h1>
+
+            <button onClick={Buscar}>Buscar</button>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Usuário</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {usuario.map(item =>
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.nome}</td>
+                            <td><Link to= {`/cadastrar/${item.id}`}>Alterar</Link></td>
+                        </tr>
+                    )}
+                </tbody>
+
+            </table>
 
         </div>
     )
